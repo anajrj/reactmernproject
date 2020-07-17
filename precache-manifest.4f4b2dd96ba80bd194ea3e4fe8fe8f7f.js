e1fc46f0f7f12da2a606470233c933dfa563b92d
anajrj <anantjaiswal28031997@gmail.com>
2020-07-17 15:34:37 +0530

    Updates

---
+++ precache-manifest.4f4b2dd96ba80bd194ea3e4fe8fe8f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c239599202d1e64eb5c31a256bc6d77",
+    "url": "/reactmernproject/index.html"
+  },
+  {
+    "revision": "dd99e3825aaf6312896d",
+    "url": "/reactmernproject/static/css/main.d8e7bbc8.chunk.css"
+  },
+  {
+    "revision": "27c3f7e6c4c802e99da8",
+    "url": "/reactmernproject/static/js/2.96eb6862.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactmernproject/static/js/2.96eb6862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd99e3825aaf6312896d",
+    "url": "/reactmernproject/static/js/main.f8dd6433.chunk.js"
+  },
+  {
+    "revision": "92f6bbf1ce854f7cb001",
+    "url": "/reactmernproject/static/js/runtime-main.ec7f93ab.js"
+  }
+]);
